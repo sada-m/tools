f44fc7879b1276f03d84ba3a77dcec01af4f5bd9
sada-m <40388027+sada-m@users.noreply.github.com>
2022-12-01 17:23:11 +0900

    Update find_update_files_by_date.php

--- find_update_files_by_date.php
+++ find_update_files_by_date.php
@@ -9,11 +9,11 @@ define('KWD_DIR',getcwd());//Must be an absolute path and end with "/"
 $aryNG=array('upload','log'); //not read file names
 echo getcwd().'<br>';
 $msg='';
-if (!isset($_GET['y']) || !isset($_GET['m']) || !isset($_GET['d'])){
+if (!isset($_GET['y']) || !isset($_GET['m']) || !isset($_GET['d']) || !isset($_GET['h'])){
 	$msg='push go button';
 }
 if ($msg==''){
-	if (!is_numeric($_GET['y']) || !is_numeric($_GET['m']) || !is_numeric($_GET['d'])){
+	if (!is_numeric($_GET['y']) || !is_numeric($_GET['m']) || !is_numeric($_GET['d']) || !is_numeric($_GET['h'])){
 		$msg='please number input!';
 	}
 }
@@ -21,12 +21,14 @@ if ($msg==''){
 	$y=$_GET['y'];
 	$m=$_GET['m'];
 	$d=$_GET['d'];
+	$h=$_GET['h'];
 	if ($y<2000 || $y>3000)$msg='please check year!';
 	if ($m<1 || $m>12)$msg='please check month!';
 	if ($d<1 || $d>31)$msg='please check day!';
+	if ($h<0 || $h>23)$msg='please check hour 0-23!';
 }
 if ($msg==''){
-	define('KIJUN_TIME',date("U",mktime(0,0,0,$m,$d,$y)));//これ以降のファイルを抽出 mktime(8,30,0,7,28,2019)の場合、2019/07/28 8:30以降ということ
+	define('KIJUN_TIME',date("U",mktime($h,0,0,$m,$d,$y)));//これ以降のファイルを抽出 mktime(8,30,0,7,28,2019)の場合、2019/07/28 8:30以降ということ
 	echo 'KIJUN_TIME='.KIJUN_TIME.'<br>'."\n";
 	$last='';
 	$last_file='';
@@ -79,10 +81,11 @@ if ($msg==''){
 		}
 	}
 ?>
-<form action="find_update_files_by_time.php" method="get">
+<form action="find_update_files_by_date.php" method="get">
 year:<input type="text" style="50px;ime-mode:disabled;" name="y" maxlength="4" value="<?php echo $y;?>">
 month:<input type="text" style="50px;ime-mode:disabled;" name="m" maxlength="2" value="<?php echo $m;?>">
 day:<input type="text" style="50px;ime-mode:disabled;" name="d" maxlength="2" value="<?php echo $d;?>">
+hour:<input type="text" style="50px;ime-mode:disabled;" name="h" maxlength="2" value="0">
 <input type="submit" value="go">
 （Exclusion:<?php echo $jogai;?>）
 </form>
@@ -90,5 +93,5 @@ day:<input type="text" style="50px;ime-mode:disabled;" name="d" maxlength="2" va
 <?php
 }
 ?>
-<br><a href="find_update_files_by_time.php">BACK</a><br>
+<br><a href="find_update_files_by_date.php">BACK</a><br>
 end
